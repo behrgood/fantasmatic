693e0ab11b065ddc114189639805abde54057a45
behrgood <behrgood@gmail.com>
2014-12-16 22:18:59 -0500

    WR Graph and RB Graph now generating successfully.

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -1,5 +1,5 @@
-function chartLaunch(data) {
-    $('#container').highcharts({
+function qbChartLaunch(data) {
+    $('#container1').highcharts({
         chart: {
             type: 'column'
         },
@@ -47,6 +47,112 @@ function chartLaunch(data) {
         }, {
             name: 'Touchdowns',
             data: data.pass_tds
+        }]
+    });
+}
+
+function rbChartLaunch(data) {
+    $('#container2').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: data.title
+        },
+        subtitle: {
+            text: data.subtitle
+        },
+        xAxis: {
+            categories: data.categories
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Statistical Data'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} </b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Attempts',
+            data: data.rush_attempts
+
+        }, {
+            name: 'Yards Per Carry',
+            data: data.rush_avg
+
+        }, {
+            name: 'Yards',
+            data: data.rush_yards
+
+        }, {
+            name: 'Touchdowns',
+            data: data.rush_tds
+        }]
+    });
+}
+
+function wrChartLaunch(data) {
+    $('#container3').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: data.title
+        },
+        subtitle: {
+            text: data.subtitle
+        },
+        xAxis: {
+            categories: data.categories
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Statistical Data'
+            }
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            name: 'Targets',
+            data: data.targets
+
+        }, {
+            name: 'Receptions',
+            data: data.receptions
+
+        }, {
+            name: 'Yards',
+            data: data.rec_yards
+
+        }, {
+            name: 'Touchdowns',
+            data: data.rec_tds
         }]
     });
 }
